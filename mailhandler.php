0b99d629ca33b3cba0bbe9cf2abd27403bca2320
revyrob <kayle.robson@gmail.com>
2022-07-20 22:53:14 -0700

    change email icon in footer and update php

--- mailhandler.php
+++ mailhandler.php
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 <?php
 header ("Location:mailto:kayle.robson@gmail.com");
 exit();
 ?>
-=======
-<?php
-header ("Location:mailto:kayle.robson@gmail.com");
-exit();
-?>
->>>>>>> 811e5e6e248cd4e10916c7355b045ab4350fd724
